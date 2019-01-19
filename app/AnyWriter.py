44d2e5d28e900e2ffbdb9833e698de238d37309e
Sean <seansnowwhite@aol.com>
2019-01-19 21:50:41 +0100

    projektcseanybody#6 output directory default in class declaration

--- app/AnyWriter.py
+++ app/AnyWriter.py
@@ -2,8 +2,9 @@ import numpy as np
 
 
 class AnyWriter:
-    def __init__(self, template_directory='config/anybody_templates'):
+    def __init__(self, template_directory='config/anybody_templates/', output_directory='../output/Anybody/'):
         self._template_directory = template_directory
+        self._output_directory = output_directory
         self.mapping = {
             'Finger1': {'joint_leap': 'RightHandThumb',
                         'joint_any': ['CMCABDUCTION', 'CMCFLEXION', 'MCPFLEXION', 'MCPABDUCTION', 'DIPFLEXION'],
@@ -45,8 +46,8 @@ class AnyWriter:
         # print(self._calctimeseries(data, entries))
 
         template_dict = {'TIMESERIES': self._joint2array(self._calctimeseries(data, entries))}
-        template_string = open('TimeSeries.template', 'r').read().format(**template_dict)
-        f = open('TimeSeries.any', 'w')
+        template_string = open(self._template_directory + 'TimeSeries.template', 'r').read().format(**template_dict)
+        f = open(self._output_directory + 'TimeSeries.any', 'w')
         f.write(template_string)
         f.close()
 
@@ -59,7 +60,7 @@ class AnyWriter:
 
             template_filename = joint_mapping['template']
             template_string = open(self._template_directory + template_filename, 'r').read().format(**template_dict)
-            f = open(finger_name + '.any', 'w')
+            f = open(self._output_directory + finger_name + '.any', 'w')
             f.write(template_string)
             f.close()
             print('"{} written"'.format(f.name))
@@ -124,52 +125,3 @@ class AnyWriter:
     @staticmethod
     def _joint2arrayneg(joint_values):
         return np.array2string(np.negative(joint_values.astype(float)), separator=', ')[1:-1]
-
-    def write_bak(self, X, ofile):
-
-        # Writing the skeleton info
-        ofile.write('HIERARCHY\n')
-
-        self.motions_ = []
-        self._printJoint(X, X.root_name, 0, ofile)
-
-        # Writing the motion header
-        ofile.write('MOTION\n')
-        ofile.write('Frames: %d\n' % X.values.shape[0])
-        ofile.write('Frame Time: %f\n' % X.framerate)
-
-        # Writing the data
-        self.motions_ = np.asarray(self.motions_).T
-        lines = [" ".join(item) for item in self.motions_.astype(str)]
-        ofile.write("".join("%s\n" % l for l in lines))
-
-    def _printJoint(self, X, joint, tab, ofile):
-
-        if X.skeleton[joint]['parent'] == None:
-            ofile.write('ROOT %s\n' % joint)
-        elif len(X.skeleton[joint]['children']) > 0:
-            ofile.write('%sJOINT %s\n' % ('\t' * (tab), joint))
-        else:
-            ofile.write('%sEnd site\n' % ('\t' * (tab)))
-
-        ofile.write('%s{\n' % ('\t' * (tab)))
-
-        ofile.write('%sOFFSET %3.5f %3.5f %3.5f\n' % ('\t' * (tab + 1),
-                                                      X.skeleton[joint]['offsets'][0],
-                                                      X.skeleton[joint]['offsets'][1],
-                                                      X.skeleton[joint]['offsets'][2]))
-        channels = X.skeleton[joint]['channels']
-        n_channels = len(channels)
-
-        if n_channels > 0:
-            for ch in channels:
-                self.motions_.append(np.asarray(X.values['%s_%s' % (joint, ch)].values))
-
-        if len(X.skeleton[joint]['children']) > 0:
-            ch_str = ''.join(' %s' * n_channels % tuple(channels))
-            ofile.write('%sCHANNELS %d%s\n' % ('\t' * (tab + 1), n_channels, ch_str))
-
-            for c in X.skeleton[joint]['children']:
-                self._printJoint(X, c, tab + 1, ofile)
-
-        ofile.write('%s}\n' % ('\t' * (tab)))
