de3c76c1c0528dc042a5682ae9d24c7060cff2e7
unknown <seansnowwhite@aol.com>
2019-03-04 19:54:57 +0100

    issue#16 feat: export position of finger tip

--- app/scratch/basis_position_anybody.py
+++ app/scratch/basis_position_anybody.py
@@ -9,82 +9,65 @@ import json
 import numpy as np
 
 
-cwd = os.getcwd()
-os.chdir('D:\\RobinSean\\Sean\\5. Master\\Projekt CSE\\AnybodyModelle\\regensburg-ulm-hand-model\\AnyBody\\Application\\Examples\\FreeHandMove')
-print(os.getcwd())
-# patella_tendon_lengths = [
-#     0.02 + i*0.01
-#     for i in range(7)
-# ]
-# print(patella_tendon_lengths)
-
 app = AnyPyProcess(num_processes=1,
                    return_task_info=True,
                    anybodycon_path="C:/Program Files/AnyBody Technology/AnyBody.7.1/AnyBodyCon.exe")
-# macrolist = [
-#     Load('Knee.any'),
-#     # SetValue('Main.MyModel.PatellaLigament.DriverPos', patella_tendon_lengths ),
-#     SetValue('Main.MyStudy.tStart', 1),
-#     SetValue('Main.MyStudy.tEnd', 2),
-#     OperationRun('Main.MyStudy.InitialConditions'),
-#     OperationRun('Main.MyStudy.Kinematics'),
-#     OperationRun('Main.MyStudy.InverseDynamics'),
-#     # Dump('Main.MyStudy.Output.Abscissa.t'),
-#     # Dump('Main.MyStudy.Output.MaxMuscleActivity'),
-#     # Dump('Main.MyModel.PatellaLigament.DriverPos'),
-# ]
-
 macrolist = [
     Load('FreeHand.Main.any'),
-    # OperationRun('Main.RunMotionAndParameterOptimizationSequence')
-    # OperationRun('Main.MyStudy.InverseDynamics'),
 ]
 
 joint_values = ['Axes0', 'r0']
+joint_prefix = 'Main.HumanModel.BodyModel.Right.ShoulderArm.Seg.'
+joints = {
+    'RightElbow': {'path': joint_prefix + 'Radius', 'values': joint_values},
+    'RightHand': {'path': joint_prefix + 'WristJointSeg', 'values': joint_values},
+    'RightHandThumb2': {'path': joint_prefix + 'Hand.Finger1.Seg.Metacarpal', 'values': joint_values},
+    'RightHandThumb3': {'path': joint_prefix + 'Hand.Finger1.Seg.ProximalPhalanx', 'values': joint_values},
+    'RightHandThumb4': {'path': joint_prefix + 'Hand.Finger1.Seg.DistalPhalanx', 'values': joint_values},
+    'RightHandThumb4_End': {'path': joint_prefix + 'Hand.Finger1.Seg.DistalPhalanx.DistalJointSurfaceNode',
+                            'values': ['r']},
+    'RightHandIndex1': {'path': joint_prefix + 'Hand.Finger2.Seg.Metacarpal', 'values': joint_values},
+    'RightHandIndex2': {'path': joint_prefix + 'Hand.Finger2.Seg.ProximalPhalanx', 'values': joint_values},
+    'RightHandIndex3': {'path': joint_prefix + 'Hand.Finger2.Seg.MiddlePhalanx', 'values': joint_values},
+    'RightHandIndex4': {'path': joint_prefix + 'Hand.Finger2.Seg.DistalPhalanx', 'values': joint_values},
+    'RightHandIndex4_End': {'path': joint_prefix + 'Hand.Finger2.Seg.DistalPhalanx.DistalJointSurfaceNode',
+                            'values': ['r']},
+    'RightHandMiddle1': {'path': joint_prefix + 'Hand.Finger3.Seg.Metacarpal', 'values': joint_values},
+    'RightHandMiddle2': {'path': joint_prefix + 'Hand.Finger3.Seg.ProximalPhalanx', 'values': joint_values},
+    'RightHandMiddle3': {'path': joint_prefix + 'Hand.Finger3.Seg.MiddlePhalanx', 'values': joint_values},
+    'RightHandMiddle4': {'path': joint_prefix + 'Hand.Finger3.Seg.DistalPhalanx', 'values': joint_values},
+    'RightHandMiddle4_End': {'path': joint_prefix + 'Hand.Finger3.Seg.DistalPhalanx.DistalJointSurfaceNode',
+                             'values': ['r']},
+    'RightHandRing1': {'path': joint_prefix + 'Hand.Finger4.Seg.Metacarpal', 'values': joint_values},
+    'RightHandRing2': {'path': joint_prefix + 'Hand.Finger4.Seg.ProximalPhalanx', 'values': joint_values},
+    'RightHandRing3': {'path': joint_prefix + 'Hand.Finger4.Seg.MiddlePhalanx', 'values': joint_values},
+    'RightHandRing4': {'path': joint_prefix + 'Hand.Finger4.Seg.DistalPhalanx', 'values': joint_values},
+    'RightHandRing4_End': {'path': joint_prefix + 'Hand.Finger4.Seg.DistalPhalanx.DistalJointSurfaceNode',
+                           'values': ['r']},
+    'RightHandPinky1': {'path': joint_prefix + 'Hand.Finger5.Seg.Metacarpal', 'values': joint_values},
+    'RightHandPinky2': {'path': joint_prefix + 'Hand.Finger5.Seg.ProximalPhalanx', 'values': joint_values},
+    'RightHandPinky3': {'path': joint_prefix + 'Hand.Finger5.Seg.MiddlePhalanx', 'values': joint_values},
+    'RightHandPinky4': {'path': joint_prefix + 'Hand.Finger5.Seg.DistalPhalanx', 'values': joint_values},
+    'RightHandPinky4_End': {'path': joint_prefix + 'Hand.Finger5.Seg.DistalPhalanx.DistalJointSurfaceNode',
+                            'values': ['r']}}
 
-# dump of joint_values for all fingers
-finger_joints = {
-    'Finger1': {'DistalPhalanx': {}, 'ProximalPhalanx': {}, 'Metacarpal': {}},
-    'Finger2': {'DistalPhalanx': {}, 'MiddlePhalanx': {}, 'ProximalPhalanx': {}, 'Metacarpal': {}},
-    'Finger3': {'DistalPhalanx': {}, 'MiddlePhalanx': {}, 'ProximalPhalanx': {}, 'Metacarpal': {}},
-    'Finger4': {'DistalPhalanx': {}, 'MiddlePhalanx': {}, 'ProximalPhalanx': {}, 'Metacarpal': {}},
-    'Finger5': {'DistalPhalanx': {}, 'MiddlePhalanx': {}, 'ProximalPhalanx': {}, 'Metacarpal': {}}}
-finger_joint_path = 'Main.HumanModel.BodyModel.Right.ShoulderArm.Seg.Hand.{}.Seg.{}.{}'
-finger_dumps = []
-for finger_name in finger_joints:
-    for joint_name in finger_joints[finger_name]:
-        for value in joint_values:
-            macrolist.append(Dump(finger_joint_path.format(finger_name, joint_name, value)))
-            finger_dumps.append({'finger_name': finger_name, 'joint_name': joint_name, 'value': value})
-    macrolist.append(Dump(finger_joint_path.format(finger_name, 'DistalPhalanx', 'DistalJointSurfaceNode') + '.r'))
-    finger_dumps.append({'finger_name': finger_name, 'joint_name': 'DistalJointSurfaceNode', 'value': 'r'})
+# Add the dumps to the macrolist
+for joint_name in joints:
+    for value in joints[joint_name]['values']:
+        macrolist.append(Dump(joints[joint_name]['path'] + '.' + value))
 
-# dump of joint_values for elbow and wrist
-other_joints = {'WristJointSeg': {},
-                'Radius': {}}
-other_joint_path = 'Main.HumanModel.BodyModel.Right.ShoulderArm.Seg.{}.{}'
-other_dumps = []
-for joint_name in other_joints:
-    for value in joint_values:
-        macrolist.append(Dump(other_joint_path.format(joint_name, value)))
-        other_dumps.append({'joint_name': joint_name, 'value': value})
+cwd = os.getcwd()
+os.chdir('D:\\RobinSean\\Sean\\5. Master\\Projekt CSE\\AnybodyModelle\\regensburg-ulm-hand-model\\AnyBody'
+         '\\Application\\Examples\\FreeHandMove')
 
 output = app.start_macro(macrolist)
 
-# save finger joints dumps into finger_joints dictionary
-for d in finger_dumps:
-    finger_joints[d['finger_name']][d['joint_name']][d['value']] = np.ndarray.tolist(
-        output[0][finger_joint_path.format(d['finger_name'], d['joint_name'], d['value'])])
-print(finger_joints)
-
-# save other joints dumps into other_joints dictionary
-for d in other_dumps:
-    other_joints[d['joint_name']][d['value']] = np.ndarray.tolist(
-        output[0][other_joint_path.format(d['joint_name'], d['value'])])
-print(other_joints)
-# parameter_study_macro = AnyMacro(macro, number_of_macros= len(patella_tendon_lengths))
 os.chdir(cwd)
 
-anybody_joint_values = finger_joints
-anybody_joint_values.update(other_joints)
-json.dump(anybody_joint_values, open('../config/anybody_joint_values.json', 'w'))
+# save as json
+result = {}
+for joint_name in joints:
+    result[joint_name] = {}
+    for value in joints[joint_name]['values']:
+        result[joint_name][value] = np.ndarray.tolist(output[0][joints[joint_name]['path'] + '.' + value])
+json.dump(result, open('../config/anybody_joint_values.json', 'w'))
