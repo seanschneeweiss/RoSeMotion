eb89c1447c29ac4cd58de39843f49b9886e66d2b
Sean Schneeweiss <seansnowwhite@aol.com>
2019-03-05 17:37:50 +0100

    issue#16 feat: rotation of local coordinate system (bones & wrist & elbow)

--- app/config/AnybodyFirstFrame.py
+++ app/config/AnybodyFirstFrame.py
@@ -3,7 +3,9 @@ import numpy as np
 
 
 # rotation matrix from anybody to LeapMotion
-ROTATION = np.array([[0, 0, -1], [0, 1, 0], [1, 0, 0]])
+ROTATION_KS = np.array([[0, 0, -1], [0, 1, 0], [1, 0, 0]])
+ROTATION_BASIS = np.array([[0, -1, 0], [0, 0, 1], [-1, 0, 0]])
+ROTATION_WRIST = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
 
 
 class AnybodyFirstFrame:
@@ -18,11 +20,16 @@ class AnybodyFirstFrame:
 
     def get_basis(self, joint_name):
         anybody_basis = self.joint_values[joint_name]['Axes0']
-        return np.matmul(ROTATION, np.array(anybody_basis))
+        # print("joint: {}, basis: \n{}".format(joint_name, np.matmul(ROTATION_BASIS, np.matmul(ROTATION_KS, np.array(anybody_basis)))))
+        if joint_name in ('RightElbow', 'RightHand'):
+            rotation = ROTATION_WRIST
+        else:
+            rotation = ROTATION_BASIS
+        return np.matmul(rotation, np.matmul(ROTATION_KS, np.transpose(np.array(anybody_basis))))
     
     def get_position(self, joint_name):
         if '_End' in joint_name:
             anybody_position = self.joint_values[joint_name]['r']
         else:
             anybody_position = self.joint_values[joint_name]['r0']
-        return np.matmul(ROTATION, np.array(anybody_position) * 1000)
+        return np.matmul(ROTATION_KS, np.array(anybody_position) * 1000)
