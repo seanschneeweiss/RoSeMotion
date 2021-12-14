000f993749ef6427c5bb5c90170003c3cab3ec9d
Sean Schneeweiss <seansnowwhite@aol.com>
2021-12-14 16:50:21 +0100

    Python wrapper for Ultraleap 5.3 Gemini, Python 3.9

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -9,8 +9,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 arch_dir = 'Gooey'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 # LeapSDK
-arch_dir = 'LeapSDK/v41_python38/lib/x64' if sys.maxsize > 2 ** 32 \
-    else 'LeapSDK/v41_python38/lib/x86'
+arch_dir = 'LeapSDK/v53_python39/lib/x64'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 # pymo
 arch_dir = 'pymo/pymo'
